a62593d41f7aeff61e4eae520bca0d7db82b2017
corpsehunter <corpsehunter@gmail.com>
2017-02-06 18:08:54 +0000

    Create init script

---
+++ init.sh
@@ -0,0 +1,5 @@
+﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+/etc/init.d/nginx restart
+ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+/etc/init.d/gunicorn restart
+﻿/etc/init.d/mysql start﻿
