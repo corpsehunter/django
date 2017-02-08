55eb5fbabecb99268e50b336dbfcc9ff2d41cf97
corpsehunter <corpsehunter@gmail.com>
2017-02-08 17:04:15 +0000

    Gunicorn service start

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo rm -rf /etc/nginx/sites-enabled/*
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+
+sudo rm -rf /etc/gunicorn.d/*
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
+sudo /etc/init.d/gunicorn restart
+
 #sudo ﻿/etc/init.d/mysql start﻿
